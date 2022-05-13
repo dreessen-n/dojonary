dca87b2b7e34ffa62d084fd9da44765dbedcd1c6
dreessen-n <dreessen.edu@gmail.com>
2022-05-12 19:00:48 -0600

    Finish function signInOut: added an if statement to switch between login and logged out

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // function to log out of site 
 
-function siteLogOut(element) {
-    console.log(element);
-
+function siteLogOut() {
+    var text = document.getElementById("btn_signInOut").innerText;
+    if (text == 'Login') {
+        document.getElementById("btn_signInOut").innerText = 'Logged Out';
+    } else {
+        document.getElementById("btn_signInOut").innerText = 'Login'
+    }
 }
